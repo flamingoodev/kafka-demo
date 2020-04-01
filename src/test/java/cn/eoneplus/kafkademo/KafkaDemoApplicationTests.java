a96efb929c5ec240877133653f4c8231cc71498b
FLAMINGO <flamingodev@outlook.com>
2020-04-01 21:05:20 +0800

    update tests

--- src/test/java/cn/eoneplus/kafkademo/KafkaDemoApplicationTests.java
+++ src/test/java/cn/eoneplus/kafkademo/KafkaDemoApplicationTests.java
@@ -1,17 +1,13 @@
 package cn.eoneplus.kafkademo;
 
-import cn.eoneplus.kafkademo.producer.KafkaService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import javax.annotation.Resource;
-
 @SpringBootTest
 class KafkaDemoApplicationTests {
-    @Resource
-    private KafkaService kafkaService;
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
+
 }
